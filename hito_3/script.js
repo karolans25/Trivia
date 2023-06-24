a92916f17fa803a406c386f4b344d7c245939d5a
Carolina Gomez <karolans@debian-BULLSEYE-live-builder-AMD64>
2023-06-24 13:16:02 -0500

    Hito 3 finalizado

--- hito_3/script.js
+++ hito_3/script.js
@@ -18,20 +18,18 @@ var opciones = [["Suspiro, turrón, alfajor", "Papajotes, torrijas, churros", "B
                ["Asado, locro, empanadas", "Pozole, cochinita pibil, tlayuda", "Mandioca, chipa, araticú"]]
 */
 
-var correctas = [2, 0, 2, 1];
+var correctas = [[2, 0, 2, 1], [1, 1, 2, 0]];
 
 var nombre = "";
-
-var numPreguntas = 4;
+var escogido = 0;
 
 function mostrar(n){
   nombre = n;
-  //alert("Hola");
-  //console.log(nombre);
 }
 
 function login(){
   document.getElementById("containerTrivia").style.display = 'none';
+  document.getElementById("containerTrivia1").style.display = 'none';
   document.getElementById("nombreL").value = "";
   document.getElementById("contestar").style.display = 'none';
   document.getElementById("reiniciar").style.display = 'none';
@@ -40,17 +38,31 @@ function login(){
   document.getElementById("bienvenida").innerHTML = "¿Quieres empezar a jugar?";
 }
 
+function empezar(){
+  document.getElementById("escoger").style.display = 'flex';
+  document.getElementById("formNombre").style.display = 'none';
+  alert ("Hola " + nombre);
+}
+
+
 function jugar(){
+  escogido = document.getElementById("selectorT").value;
+  document.getElementById("escoger").style.display = 'none';
+  if (escogido == 0) {
+    document.getElementById("containerTrivia").style.display = 'flex';
+  } else if (escogido == 1){
+    document.getElementById("containerTrivia1").style.display = 'flex';
+  }
+  
   //let nombre = prompt ("¿Cómo te llamas?");
   document.getElementById("bienvenida").style.display = 'block';
   const textNombre = document.getElementById("nombreL");
   //nombre = nombre.toUpperCase().toLowerCase();
   //nombre = nombre.charAt(0).toUpperCase().concat(nombre.substring(1, nombre.length));
   textNombre.value = nombre;
-  alert ("Hola " + textNombre.value);
   const msg = document.getElementById("bienvenida");
   msg.innerHTML = `¡Bienvenida, ${nombre}! Ahora si, ¡vamos a jugar!`;
-  document.getElementById("containerTrivia").style.display = 'flex';
+  //document.getElementById("containerTrivia").style.display = 'flex';
   document.getElementById("contestar").style.display = 'flex';
   document.getElementById("formNombre").style.display = 'none';
 
@@ -70,52 +82,79 @@ function jugar(){
     }
   }
 
-  var ps = document.getElementsByTagName("p");
-  for (var p of ps){
-    if (p.id.startsWith("pregunta-")){
-      var div = p.textContent.split("\t");
-      p.textContent = div[0];
+  
+  var pEmojis = document.getElementsByTagName("p");
+  for (var i of pEmojis){
+    if(i.id.startsWith("emoji-")){
+      i.innerHTML = "";
     }
   }
+  
 }
 
 function contestar() {
   /* Lógica del proceso */
   var selected = false;
   
+  var puntaje = 0;
+  var sumar = 100/(correctas[escogido].length);
 
-  for (let i=0; i<numPreguntas; i++){
-    var pre = `pregunta-${i+1}`;
+  let i = 0;
+  let max = correctas[escogido].length;
+  if (escogido == 1){
+    i = max;
+    max = i + max;
+  }
+
+  for (i; i<max; i++){
+    var pre = `pregunta-${parseInt(i)+1}`;
     var pregunta = document.getElementById(pre);
     var radios = document.getElementsByName(pre);
-    for (var j in radios){
-      // Proceso de respuesta del usuario
-      if (radios[j].type === 'radio' && radios[j].checked){
-        var labelCorrecta = document.getElementById(`op-${parseInt(i)+1}.${correctas[i]+1}`);
+    
+    var emoji = `emoji-${parseInt(i)+1}`;
+    var pEmoji = document.getElementById(emoji);
+
+    for (var r of radios){
+      // Proceso de evaluación
+      if (r.type === 'radio' && r.checked){
+        var buscarCorrecta = `op-${parseInt(i)+1}.${correctas[escogido][parseInt(i)] + 1}`;
+        if (escogido == 1){
+          buscarCorrecta = `op-${parseInt(i)+1}.${correctas[escogido][parseInt(i)-correctas[escogido].length]+1}`;
+        }
+        var labelCorrecta = document.getElementById(buscarCorrecta);
         labelCorrecta.style.backgroundColor = "green";
         labelCorrecta.style.color = "white";
 
-        var labelRadio = document.getElementById(`op-${i+1}.${parseInt(j)+1}`);
-        if (j != correctas[i]){
-          labelRadio.style.backgroundColor = "red";
-          labelRadio.style.color = "yellow";
-          pregunta.append("\t🥺 ❌");
-        } else if (j == correctas[i]){
-          pregunta.append("\t🥰 ✅");
+        var buscarSeleccionada = `op-${parseInt(i)+1}.${parseInt(r.value) + 1}`;
+        var labelSeleccionada = document.getElementById(buscarSeleccionada);
+        
+        var comparar = correctas[escogido][i];
+        if (escogido == 1){
+          comparar = correctas[escogido][parseInt(i)-correctas[escogido].length];
+        }
+        if (parseInt(r.value) != comparar){
+          labelSeleccionada.style.backgroundColor = "red";
+          labelSeleccionada.style.color = "yellow";
+          pEmoji.innerHTML = "\t🥺 ❌";
+        } else if (parseInt(r.value) == comparar){
+          pEmoji.innerHTML = "\t🥰 ✅";
+          puntaje += sumar;
         }
         selected = true;
       }
-      if (radios[j].type === 'radio'){
-        radios[j].setAttribute('disabled', 'disabled');
+      if (r.type === 'radio'){
+        r.setAttribute('disabled', 'disabled');
       }
     }
     if (!selected) {
-      pregunta.append("\t💬 ❌");
+      pEmoji.innerHTML = "\t💬 ❌";
       //labelCorrecta.style.backgroundColor = "green";
       //labelCorrecta.style.color = "white";
     }
     selected = false;
-  }      
-    document.getElementById("contestar").style.display = 'none';
-    document.getElementById("reiniciar").style.display = 'flex';
+
+  }
+  alert(`\t\tGracias por jugar la Trivia:\n\t\t\t\tTu puntaje es ${puntaje}/100`)
+  document.getElementById("contestar").style.display = 'none';
+  document.getElementById("reiniciar").style.display = 'flex';
 }
